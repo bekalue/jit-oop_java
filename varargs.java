435c4eed6670f8611fa724ea3698e1c665e5b993
Bekalu Geta <bekalu2054@gmail.com>
2022-12-09 14:38:36 +0300

    another banger

---
+++ varargs.java
@@ -0,0 +1,20 @@
+public class varargs
+{
+    public static int product(int... x)
+    {
+        //this method return the product of passed arguments.
+        int product = 1;
+
+        for (int i: x)
+        {
+            product *= i;
+        }
+        return product;
+    }
+    public static void main(String[] args)
+    {
+        int x;
+        x = product(3, 4);
+        System.out.println(x);
+    }
+}
