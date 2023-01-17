0e68e59b653ef7f045aa5eef0165483cc89805e9
Bekalu Geta <bekalu2054@gmail.com>
2023-01-17 14:32:07 +0300

    small changes

---
+++ lab/Animal.java
@@ -0,0 +1,15 @@
+package animal;
+
+interface World
+{
+	void numOfSpecies();
+	void calculateTotal(int...r);
+	void canMove();
+	void reptils();
+}
+abstract class Animal{
+	String name;
+	int x;
+	abstract int gotNumber(int ...y){
+b         	}
+
